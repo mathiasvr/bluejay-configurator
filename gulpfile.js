3fb5365845d5a1089bdac371cc2041c92590a367
Mathias Rasmussen <mathiasvr@gmail.com>
2021-04-10 20:06:19 +0200

    Update nw.js to 0.52.2

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ var debugDir = './debug/';
 var releaseDir = './release/';
 var jsBuildDir = './js/build/'
 
-const nwVersion = '0.52.1';
+const nwVersion = '0.52.2';
 
 // -----------------
 // Helper functions
